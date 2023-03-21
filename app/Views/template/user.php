ccdd231e352da934afbdb4663591454ad3f90f76
Halomoan Filipus Simarmata <moanfs1@gmail.com>
2023-03-21 14:44:59 +0700

    first commit

---
+++ app/Views/template/user.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <title>Donor Darah</title> -->
+    <?= $this->renderSection('title'); ?>
+    <link rel="stylesheet" href="<?= base_url(); ?>/assets/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/node_modules/css/style.css">
+    <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
+</head>
+
+<body>
+    <section class="container-fluid">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="assets/img/logo.png" alt="Logo" width="28" class="d-inline-block align-text-top">
+                    donortree
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarText">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Tentang Kami</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Stok Darah</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Berita</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Info Donor
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Kontak</a>
+                        </li>
+                    </ul>
+                    <ul class="narbar-nav ms-auto mb-2 mb-lg-0">
+                        <button class="btn btn-outline-danger btn-sm">Sign in</button>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+    <!-- Main Content -->
+    <div class="main-content">
+        <?= $this->renderSection('content'); ?>
+    </div>
+    <!-- end Content -->
+    <script src="/assets/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ app/Views/user/dashboard.php
@@ -0,0 +1,33 @@
+<?= $this->extend('template/user') ?>
+<!-- title -->
+<?= $this->Section('title') ?>
+<title>Beranda &mdash; Donor Darah</title>
+<?= $this->endSection(); ?>
+
+<?= $this->Section('content'); ?>
+<div class="container text-center mt-5">
+    <div class="row">
+        <div class="col-6">
+            <img src="assets/img/gambar1.png" height="500" alt="">
+        </div>
+        <div class="col-6">
+            <p class="m-auto">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim modi mollitia nam eum obcaecati, commodi rem iusto. Veritatis laudantium voluptate magni, voluptatibus inventore perferendis similique rem nam modi, aut nihil voluptates eos, excepturi molestiae tempore adipisci incidunt sapiente aspernatur? Praesentium similique officia eum aut tempora ex ratione eaque, vitae fugiat.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
+
+<div class="about">
+    <h2>about us</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est atque itaque quas veritatis officiis debitis obcaecati! Optio odit dicta deleniti, officiis vel laboriosam quod, nemo officia alias ea quam iste.</p>
+</div>
+<?= $this->endSection(); ?>
