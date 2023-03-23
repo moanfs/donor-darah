8e20729e06fee26e063d5f677e1b75983fea2313
Halomoan Filipus Simarmata <moanfs1@gmail.com>
2023-03-23 18:00:15 +0700

    update css

--- app/Views/user/dashboard.php
+++ app/Views/user/dashboard.php
@@ -5,23 +5,40 @@
 <?= $this->endSection(); ?>
 
 <?= $this->Section('content'); ?>
-<div class="container text-center mt-5">
+<div class="container mt-5">
     <div class="row">
-        <div class="col-6">
-            <img src="assets/img/gambar1.png" height="500" alt="">
+        <div class="col-sm-6 col-md-6">
+            <img class="img-fluid" src="assets/img/gambar1.png" alt="">
         </div>
-        <div class="col-6">
-            <p class="m-auto">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim modi mollitia nam eum obcaecati, commodi rem iusto. Veritatis laudantium voluptate magni, voluptatibus inventore perferendis similique rem nam modi, aut nihil voluptates eos, excepturi molestiae tempore adipisci incidunt sapiente aspernatur? Praesentium similique officia eum aut tempora ex ratione eaque, vitae fugiat.</p>
+        <div class="col-sm-5 offset-sm-2 col-md-5 m-auto">
+            <h4>Langka Kecil Untuk Memberi</h4>
+            <h2>Dampak Besar</h2>
+            <p>Ayo donor darah sekarang dan berikan dampak yang besar bagi yang membutuhkan</p>
+            <a href="#" class="btn btn-danger rounded-4">DONATE NOW</a>
         </div>
     </div>
 </div>
 
-<div class="container">
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="container mt-5">
+    <div class="card text-bg-danger rounded-5">
+        <div class="card-body ">
+            <div class="row text-center p-3">
+                <div class="col-sm-4 col-md-4">
+                    <h5>Donor</h5>
+                    <h1>20.000</h1>
+                </div>
+
+                <div class="col-sm-4 col-md-4">
+                    <h5>People needed</h5>
+                    <h1>10.000</h1>
+                </div>
+
+                <div class="col-sm-4 col-md-4">
+                    <h5>People saved</h5>
+                    <h1>90.000</h1>
+                </div>
+
+            </div>
         </div>
     </div>
 </div>
